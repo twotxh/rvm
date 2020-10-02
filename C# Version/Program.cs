5ad87cc7408a93c59af002f7e15176d3c876ade9
Carpal__ <git@github.com:Carpall/RobinScript.git>
2020-10-02 18:06:17 +0200

    extra

--- C# Version/Program.cs
+++ C# Version/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 class Program {
+    static void makewhatiwant() {
+        Console.WriteLine("");
+    }
     static void Main(string[] args) {
         var main = new Instruction[] {
-            Instruction.New(Runtime.Load, 12),
-            Instruction.New(Runtime.Load, 11),
-            Instruction.New(Runtime.CompareLess),
-            Instruction.New(Runtime.RvmOutput)
+            Instruction.New(Runtime.Load, new Runtime.builtinCall(makewhatiwant)),
+            Instruction.New(Runtime.RvmCall)
         };
         var program = new Group[] {
             Group.New(main),
         };
+        Console.WriteLine("{0}\n", DateTime.Now);
         Rvm.Execute(program);
+        Console.WriteLine("\n{0}",DateTime.Now);
     }
 }
