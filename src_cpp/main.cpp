7e541d294734c6db4b214d80a3b71cb0667b1539
carpall <juniormondi@gmail.com>
2020-10-18 11:35:30 +0200

    implementing runtime object

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -1,45 +1,19 @@
 ﻿#include <vector>
-#include "rvm.h"
-class Int : RuntimeObject {
-    int value = 0;
-    void add(RuntimeObject obj) override {
-        value += obj.to_int();
-    }
-    void add(int val) override {
-        value += val;
-    }
-    void sub(int val) override {
-        value -= val;
-    }
-    void mul(int val) override {
-        value *= val;
-    }
-    void div(int val) override {
-        value /= val;
-    }
-    int to_int() override {
-        return value;
-    }
-    enum type type() override {
-        return type::_int;
-    }
-};
-void put(RuntimeObject chr) {
-    std::cout.put(chr.to_char());
-}
+#include "rvm.cpp"
 int main() {
-
-    const std::vector<instruction> main = {
-        instruction(rvm::runtime::call, 1),
-        instruction(rvm::runtime::ret),
-    };
-    const std::vector<instruction> printhelloro = {
-        instruction(rvm::runtime::load, Int()),
-        instruction(rvm::runtime::ret),
-    };
-    std::vector<group> program {
-        group(main),
-        group(printhelloro),
-    };
-    rvm::execute(program);
+    Int x = 10;
+    std::cout<<x.to_int();
+//    const std::vector<instruction> main = {
+//        instruction(rvm::runtime::call, 1),
+//        instruction(rvm::runtime::ret),
+//    };
+//    const std::vector<instruction> printhelloro = {
+//        instruction(rvm::runtime::load, Int()),
+//        instruction(rvm::runtime::ret),
+//    };
+//    std::vector<group> program {
+//        group(main),
+//        group(printhelloro),
+//    };
+//    rvm::execute(program);
 }
--- src_cpp/runtime_object.h
+++ src_cpp/runtime_object.h
@@ -1,6 +1,7 @@
 enum type {
     _string,
     _int,
+    _bool,
     _float,
     _char,
     _runtime_object
@@ -10,24 +11,82 @@ struct RuntimeObject {
     virtual int to_int();
     virtual char to_char();
     virtual float to_float();
-    virtual void set();
-    virtual void sub(RuntimeObject obj);
-    virtual void add(RuntimeObject obj);
-    virtual void mul(RuntimeObject obj);
-    virtual void div(RuntimeObject obj);
-    virtual void sub(int val);
-    virtual void add(int val);
-    virtual void mul(int val);
-    virtual void div(int val);
-    virtual void sub(float val);
-    virtual void add(float val);
-    virtual void mul(float val);
-    virtual void div(float val);
-    virtual void add(const char* val);
-    virtual void add(char val);
+    virtual float to_bool();
+    virtual RuntimeObject set();
+    virtual RuntimeObject sub(RuntimeObject obj);
+    virtual RuntimeObject add(RuntimeObject obj);
+    virtual RuntimeObject mul(RuntimeObject obj);
+    virtual RuntimeObject div(RuntimeObject obj);
+    virtual RuntimeObject sub(int val);
+    virtual RuntimeObject add(int val);
+    virtual RuntimeObject mul(int val);
+    virtual RuntimeObject div(int val);
+    virtual RuntimeObject sub(float val);
+    virtual RuntimeObject add(float val);
+    virtual RuntimeObject mul(float val);
+    virtual RuntimeObject div(float val);
+    virtual RuntimeObject add(const char* val);
+    virtual RuntimeObject add(char val);
     virtual type type();
     virtual bool is_string();
     virtual bool is_int();
     virtual bool is_char();
     virtual bool is_float();
+    virtual bool is_bool();
+    virtual bool equals(RuntimeObject obj);
+    virtual bool greater(RuntimeObject obj);
+    virtual bool less(RuntimeObject obj);
+    virtual bool notequals(RuntimeObject obj);
+    virtual bool operator !() {
+        return !(this->is_bool() && this->to_bool());
+    }
+    virtual bool operator ==(bool expr) {
+        return this->is_bool() && this->to_bool() == expr;
+    }
+};
+
+
+class Bool : public RuntimeObject {
+public:
+    bool value;
+    Bool(bool val) {
+        value = val;
+    }
+};
+class Int : public RuntimeObject {
+public:
+    int value = 0;
+    RuntimeObject add(RuntimeObject obj) override {
+        value += obj.to_int();
+        return Int(value);
+    }
+    RuntimeObject add(int val) override {
+        value += val;
+        return Int(value);
+    }
+    RuntimeObject sub(int val) override {
+        value -= val;
+        return Int(value);
+    }
+    RuntimeObject mul(int val) override {
+        value *= val;
+        return Int(value);
+    }
+    RuntimeObject div(int val) override {
+        value /= val;
+        return Int(value);
+    }
+    int to_int() override {
+        return value;
+    }
+    enum type type() override {
+        return type::_int;
+    }
+    Int(int val) {
+        value = val;
+    }
+    Int operator =(int val) {
+        value = val;
+        return Int(val);
+    }
 };
